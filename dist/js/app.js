56559d8125fbdb0c4c82ee3dcf293f70724f44ac
gorejkoff <neskazua63@gmail.com>
2025-11-14 08:15:22 +0300

    first commit

---
+++ dist/js/app.js
@@ -0,0 +1,309 @@
+"use strict"
+
+// window.addEventListener('load', (event) => {});
+
+// desktop or mobile (mouse or touchscreen)
+const isMobile = {
+   Android: function () { return navigator.userAgent.match(/Android/i) },
+   BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i) },
+   iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i) },
+   Opera: function () { return navigator.userAgent.match(/Opera Mini/i) },
+   Windows: function () { return navigator.userAgent.match(/IEMobile/i) },
+   any: function () {
+      return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+   }
+};
+const isPC = !isMobile.any();
+if (isPC) { document.body.classList.add('_pc') } else { document.body.classList.add('_touch') };
+
+// media queries
+const MIN1024 = window.matchMedia('(min-width: 1024px)');
+const MIN768 = window.matchMedia('(min-width: 768px)');
+
+// variables
+const HEADER = document.getElementById('header');
+
+
+
+function throttle(callee, timeout) {
+   let timer = null;
+   return function perform(...args) {
+      if (timer) return;
+      timer = setTimeout(() => {
+         callee(...args);
+         clearTimeout(timer);
+         timer = null;
+      }, timeout)
+   }
+}
+
+
+
+/* запись переменных высоты элементов */
+// function addHeightVariable() {
+//    if (typeof HEADER !== "undefined") {
+//       document.body.style.setProperty('--height-header', `${HEADER.offsetHeight}px`)
+//    }
+// }
+// addHeightVariable();
+
+
+// ** ======================= RESIZE ======================  ** //
+window.addEventListener('resize', () => {
+   //  addHeightVariable();
+   closeHeaderMenu();
+})
+
+
+// ** ======================= CLICK ======================  ** //
+document.documentElement.addEventListener("click", (event) => {
+   if (event.target.closest('.open-menu')) { openHeaderMenu() }
+})
+
+
+function openHeaderMenu() {
+   document.body.classList.toggle('menu-is-open')
+}
+function closeHeaderMenu() {
+   document.body.classList.remove('menu-is-open')
+}
+
+
+if (document.querySelector('.banner-video video')) {
+   let callback = function (entries, observer) {
+      entries.forEach((entry) => {
+         if (entry.isIntersecting) {
+            entry.target.play();
+            return
+         }
+         entry.target.pause();
+      })
+   };
+   let observer = new IntersectionObserver(callback, { threshold: 0.2 });
+   let target = document.querySelectorAll('.banner-video video');
+   target.forEach(event => observer.observe(event));
+}
+
+
+if (document.querySelector('.banner-fade__swiper')) {
+   const element = document.querySelector('.banner-fade__swiper');
+   const buttonNext = element.querySelector('.button-next')
+   const buttonPrev = element.querySelector('.button-prev')
+   const delay = 3000;
+   const speed = 500;
+   const swiper = new Swiper(element, {
+      // spaceBetween: 30,
+      allowTouchMove: true,
+      // loop: true,
+      speed: speed,
+      slidesPerView: 1,
+      effect: "fade",
+      fadeEffect: {
+         crossFade: false,
+      },
+      // autoplay: {
+      //    delay: delay,
+      // },
+      navigation: {
+         nextEl: buttonNext,
+         prevEl: buttonPrev,
+      },
+   });
+
+   const nextSlider = () => { swiper.slideNext(speed) }
+   const toBeginning = () => {
+      clearTimeout(nextSliderTime)
+      swiper.slideTo(0, speed)
+   }
+   let nextSliderTime = null;
+   let toBeginningTime = null;
+   swiper.on('slideChange', () => {
+      nextSliderTime = setTimeout(nextSlider, delay)
+   })
+   swiper.on('reachEnd', () => {
+      clearTimeout(nextSliderTime)
+      toBeginningTime = setTimeout(toBeginning, delay)
+   })
+   const brforeClick = () => {
+      clearTimeout(nextSliderTime);
+   }
+   buttonPrev.addEventListener('click', () => { brforeClick() });
+   buttonNext.addEventListener('click', () => { brforeClick() });
+   setTimeout(nextSlider, delay);
+}
+
+/* пример инициализации слайдера */
+// if (document.querySelector('.swiper')) {
+//    const swiper = new Swiper('.swiper', {
+//       keyboard: {
+//          enabled: true,
+//          onlyInViewport: true,
+//       },
+//       allowTouchMove: false,
+//       loop: true,
+//       spaceBetween: 10,
+//       speed: 300,
+//       slidesPerView: 2.5,
+//       slidesPerView: 'auto', // количаство слайдеров без авто ширины
+//       grabCursor: true,
+//       initialSlide: 2,
+//       centeredSlides: true,
+//       breakpoints: {
+//          1024: {
+//             spaceBetween: 20,
+//             slidesPerView: 3
+//          },
+//          768: {
+//             slidesPerView: 2
+//          }
+//       },
+//       navigation: {
+//          nextEl: ".next",
+//          prevEl: ".prev",
+//       },
+//       pagination: {
+//          el: '.pagination__body',
+//          type: 'bullets',
+//          type: 'fraction',
+//          clickable: true,
+//       },
+//       autoplay: {
+//          delay: 2000,
+//       },
+//       virtual: {
+//          enabled: true,
+//       },
+//       freeMode: {
+//          enabled: true,
+//          momentum: false // Отключаем инерцию для точного позиционирования
+//       },
+//    });
+// }
+
+
+
+
+/* создание и ликвидация состояния слайдера в зависимости от ширины вьюпорта */
+// if (document.querySelector('.swiper')) {
+//    let swiperState;
+//    let swiper;
+//    changeStateSlider();
+//    window.addEventListener('resize', () => {
+//       changeStateSlider();
+//    })
+//    function initswiper() {
+//       swiper = new Swiper('.swiper', {
+//          keyboard: {
+//             enabled: true,
+//             onlyInViewport: true,
+//          },
+//          allowTouchMove: true,
+//          loop: false,
+//          speed: 300,
+//          slidesPerView: 1.3,
+//          spaceBetween: 24,
+//       });
+//    }
+//    function changeStateSlider() {
+//       if (!MIN768.matches) {
+//          if (!swiperState) {
+//             swiperState = true;
+//             initswiper();
+//          }
+//       } else {
+//          if (swiperState) {
+//             swiperState = false;
+//             swiper.destroy(true, true);
+//          }
+//       }
+//    }
+// }
+
+// js-tabs-body - тело вкладки, в открытом состоянии добавляется класс js-tabs-open.
+// js-tabs-hover - работает hover на ПК, отключает клик на ПК, для touchscreen надо раставить js-tabs-click или js-tabs-toggle
+// js-tabs-closing - вместе с js-tabs-bod закрыть вкладку при событии вне данной вкладки
+// js-tabs-click - открыть при клике (зона клика)
+// js-tabs-toggle - открыть или закрыть при клике (зона клика)
+// js-tabs-shell - оболочка скрывающая js-tabs-inner
+// js-tabs-inner - оболочка контента
+// 
+// 
+//  работает в связке с определением touchscreen  (isPC)
+
+
+class Tabs {
+   constructor() {
+      this.listClosingTabs = document.querySelectorAll('.js-tabs-closing');
+      this.listHover = document.querySelectorAll('.js-tabs-hover');
+      this.listTabsBody = document.querySelectorAll('.js-tabs-body');
+   };
+   init = () => {
+      document.body.addEventListener('click', this.eventClick);
+      if (isPC) document.body.addEventListener('mouseover', this.eventMouseOver)
+      window.addEventListener('resize', this.resize);
+   };
+   eventMouseOver = (event) => {
+      if (event.target.closest('.js-tabs-hover')) this.openTabs(event);
+      this.closeAllHover(event);
+   };
+   eventClick = (event) => {
+      if (isPC && event.target.closest('.js-tabs-hover')) return;
+      this.closeAll(event);
+      if (event.target.closest('.js-tabs-click')) this.openTabs(event);
+      if (event.target.closest('.js-tabs-toggle')) this.toggleTabs(event);
+   };
+   openTabs = (event) => {
+      const body = event.target.closest('.js-tabs-body');
+      if (!body) return;
+      body.classList.add('js-tabs-open');
+      this.setHeight(body);
+   };
+   closeTabs = (body) => {
+      body.classList.remove('js-tabs-open');
+      this.clearHeight(body);
+   };
+   closeAll = (event) => {
+      const body = event.target.closest('.js-tabs-body');
+      if (this.listClosingTabs.length == 0 && body) return;
+      this.listClosingTabs.forEach((e) => { if (e !== body) this.closeTabs(e); })
+   };
+   closeAllHover = (event) => {
+      const body = event.target.closest('.js-tabs-hover');
+      if (this.listHover.length == 0 && body) return;
+      this.listHover.forEach((e) => { if (e !== body) this.closeTabs(e) })
+   };
+   setHeight = (body) => {
+      const heightValue = body.querySelector('.js-tabs-inner').offsetHeight;
+      body.querySelector('.js-tabs-shell').style.height = heightValue + "px";
+   };
+   clearHeight = (body) => { body.querySelector('.js-tabs-shell').style.height = "" }
+   toggleTabs = (event) => {
+      const body = event.target.closest('.js-tabs-body');
+      if (body.classList.contains('js-tabs-open')) {
+         this.closeTabs(body);
+         return;
+      }
+      this.openTabs(event);
+   };
+   throttle = () => {
+      let timer = null;
+      return () => {
+         if (timer) return;
+         timer = setTimeout(() => {
+            const unlocked = document.querySelectorAll('.js-tabs-open');
+            unlocked.forEach((e) => { this.setHeight(e) })
+            clearTimeout(timer);
+            timer = null;
+         }, 100)
+      }
+   };
+   resize = this.throttle();
+}
+const tabs = new Tabs();
+tabs.init();
+
+
+
+
+
+
---
+++ src/js/modules_js/swiper.js
@@ -0,0 +1,134 @@
+
+if (document.querySelector('.banner-fade__swiper')) {
+   const element = document.querySelector('.banner-fade__swiper');
+   const buttonNext = element.querySelector('.button-next')
+   const buttonPrev = element.querySelector('.button-prev')
+   const delay = 3000;
+   const speed = 500;
+   const swiper = new Swiper(element, {
+      // spaceBetween: 30,
+      allowTouchMove: true,
+      // loop: true,
+      speed: speed,
+      slidesPerView: 1,
+      effect: "fade",
+      fadeEffect: {
+         crossFade: false,
+      },
+      // autoplay: {
+      //    delay: delay,
+      // },
+      navigation: {
+         nextEl: buttonNext,
+         prevEl: buttonPrev,
+      },
+   });
+
+   const nextSlider = () => { swiper.slideNext(speed) }
+   const toBeginning = () => {
+      clearTimeout(nextSliderTime)
+      swiper.slideTo(0, speed)
+   }
+   let nextSliderTime = null;
+   let toBeginningTime = null;
+   swiper.on('slideChange', () => {
+      nextSliderTime = setTimeout(nextSlider, delay)
+   })
+   swiper.on('reachEnd', () => {
+      clearTimeout(nextSliderTime)
+      toBeginningTime = setTimeout(toBeginning, delay)
+   })
+   const brforeClick = () => {
+      clearTimeout(nextSliderTime);
+   }
+   buttonPrev.addEventListener('click', () => { brforeClick() });
+   buttonNext.addEventListener('click', () => { brforeClick() });
+   setTimeout(nextSlider, delay);
+}
+
+/* пример инициализации слайдера */
+// if (document.querySelector('.swiper')) {
+//    const swiper = new Swiper('.swiper', {
+//       keyboard: {
+//          enabled: true,
+//          onlyInViewport: true,
+//       },
+//       allowTouchMove: false,
+//       loop: true,
+//       spaceBetween: 10,
+//       speed: 300,
+//       slidesPerView: 2.5,
+//       slidesPerView: 'auto', // количаство слайдеров без авто ширины
+//       grabCursor: true,
+//       initialSlide: 2,
+//       centeredSlides: true,
+//       breakpoints: {
+//          1024: {
+//             spaceBetween: 20,
+//             slidesPerView: 3
+//          },
+//          768: {
+//             slidesPerView: 2
+//          }
+//       },
+//       navigation: {
+//          nextEl: ".next",
+//          prevEl: ".prev",
+//       },
+//       pagination: {
+//          el: '.pagination__body',
+//          type: 'bullets',
+//          type: 'fraction',
+//          clickable: true,
+//       },
+//       autoplay: {
+//          delay: 2000,
+//       },
+//       virtual: {
+//          enabled: true,
+//       },
+//       freeMode: {
+//          enabled: true,
+//          momentum: false // Отключаем инерцию для точного позиционирования
+//       },
+//    });
+// }
+
+
+
+
+/* создание и ликвидация состояния слайдера в зависимости от ширины вьюпорта */
+// if (document.querySelector('.swiper')) {
+//    let swiperState;
+//    let swiper;
+//    changeStateSlider();
+//    window.addEventListener('resize', () => {
+//       changeStateSlider();
+//    })
+//    function initswiper() {
+//       swiper = new Swiper('.swiper', {
+//          keyboard: {
+//             enabled: true,
+//             onlyInViewport: true,
+//          },
+//          allowTouchMove: true,
+//          loop: false,
+//          speed: 300,
+//          slidesPerView: 1.3,
+//          spaceBetween: 24,
+//       });
+//    }
+//    function changeStateSlider() {
+//       if (!MIN768.matches) {
+//          if (!swiperState) {
+//             swiperState = true;
+//             initswiper();
+//          }
+//       } else {
+//          if (swiperState) {
+//             swiperState = false;
+//             swiper.destroy(true, true);
+//          }
+//       }
+//    }
+// }
