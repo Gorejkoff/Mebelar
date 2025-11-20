2c3f5217890dc899fca870610d1fe68090cd594a
gorejkoff <neskazua63@gmail.com>
2025-11-20 13:39:53 +0300

    add page news

--- dist/js/app.js
+++ dist/js/app.js
@@ -198,6 +198,40 @@ if (document.querySelector('.js-count')) {
       }
    }
 }
+// map
+if (document.getElementById('map')) {
+   initMap();
+   async function initMap() {
+      await ymaps3.ready;
+      const { YMap, YMapDefaultSchemeLayer } = ymaps3;
+      const map = new YMap(
+         document.getElementById('map'),
+         {
+            location: {
+               center: [37.63577597309541, 55.71973092218984],
+               zoom: 17,
+               duration: 200,
+            }
+         }
+      );
+
+      map.addChild(new YMapDefaultSchemeLayer());
+      document.addEventListener('click', (event) => {
+         if (event.target.closest('.contacts__cell-button')) {
+            const coordinates = event.target.closest('.contacts__cell-button').dataset.map;
+            if (coordinates) {
+               map.update({
+                  location: {
+                     center: coordinates.split(','),
+                     zoom: 17,
+                     duration: 200,
+                  }
+               })
+            }
+         }
+      })
+   }
+}
 /* открывает, закрывает модальные окна. */
 /*
 добавить классы
